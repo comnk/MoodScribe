7b24b0b26e8440b7f368bfbc66458eff697ff9b1
comnk <vhsiao36@gmail.com>
2024-05-14 22:04:48 -0700

    small changes

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from flask import Flask, render_template
 import pymongo
 import bcrypt
+import os
 
 app = Flask(__name__, template_folder = 'templates')
 
-client = pymongo.MongoClient("mongodb+srv://admin:ruLwEaJeXrddZVtm@cluster0.r7sglr2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = pymongo.MongoClient(os.environ.get("MONGODB_URI"))
 db = client.get_database("total_records")
 records = db.register
-
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
 @app.route("/")
 def index():
